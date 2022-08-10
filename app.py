f1be733206d35f9bf466bd9c2d58a2be5681f47d
Daniel Yamakov <yamakov03@gmail.com>
2022-08-10 00:45:23 -0500

    refactor

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, session, app
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from flask_ckeditor import CKEditor
+from flask_wtf.csrf import CSRFProtect
+from datetime import timedelta
+
+ckeditor = CKEditor()
+csrf = CSRFProtect()
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'M@$H238p4fmOIsdklfnkadjsfh9uq456wvhdefgF@$Ff4575867jfghve@%$Gsdfgd5780ndfht36vu4'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}.sqlite'
+    db.init_app(app)
+    csrf.init_app(app)
+
+    ckeditor.init_app(app)
+
+    from views import views
+    from auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    @app.before_request
+    def make_session_permanent():
+        session.permanent = True
+        app.permanent_session_lifetime = timedelta(minutes=5)
+
+    return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
+
---
+++ static/index.js
@@ -0,0 +1,26 @@
+function deleteNote(noteId) {
+    fetch('/delete-note', {
+        method: 'POST',
+        body: JSON.stringify({noteId: noteId})
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
+
+function editNote(noteId) {
+    window.location.href = "/edit-note/" + noteId;
+}
+
+function duplicateNote(noteId) {
+    fetch('/duplicate-note', {
+        method: 'POST',
+        body: JSON.stringify({noteId: noteId})
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
+
+function returnHome() {
+    window.location.href = "/";
+}
+
