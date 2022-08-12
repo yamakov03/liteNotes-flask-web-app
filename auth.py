dc252bcfa28685e2e557a0ee6105669ce3ecfb5e
Daniel Yamakov <yamakov03@gmail.com>
2022-08-12 02:43:26 -0500

    session

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
@@ -20,8 +20,9 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
+                session.permanent = True                
+                flash('Logged in successfully!', category='success')                
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -63,6 +64,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
+            session.permanent = True
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
