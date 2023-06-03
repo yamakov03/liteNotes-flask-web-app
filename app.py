493462c40c9a17c18e2699926261c848ef1738e0
Daniel Yamakov <yamakov03@gmail.com>
2023-06-03 12:28:09 -0500

    db changes and config updates

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_ckeditor import CKEditor
 from flask_wtf.csrf import CSRFProtect
 from datetime import timedelta
 from flask_talisman import Talisman
+import uuid
 
 ckeditor = CKEditor()
 csrf = CSRFProtect()
@@ -16,10 +17,10 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'M@$H238p4fmOIsdklfnkadjsfh9uq456wvhdefgF@$Ff4575867jfghve@%$Gsdfgd5780ndfht36vu4'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}.sqlite'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-    app.config['ALLOWED_HOSTS'] = ["litenotes.herokuapp.com"]
+    app.config['SECRET_KEY'] = uuid.uuid4().hex
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    app.config['ALLOWED_HOSTS'] = ["lite-notes.vercel.app"]
     app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=5)
     
     db.init_app(app)
